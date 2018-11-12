ae6ac061a2ebca74f0dc2aff6d99accaec3a3efd
Brooked <dauguetjulien@gmail.com>
2018-11-12 16:04:48 +0100

    script js in indexJS

--- projetweb/public/indexJS.js
+++ projetweb/public/indexJS.js
@@ -1,13 +1,7 @@
-Vue.prototype.$http = axios
 
-
-    this.$http.get('/articles')
-        .then(list => {
-            console.log('affichage list ',list)
-            this.articleslist = liste.data
-        })
-        .catch(err => {
-            console.log('error',err)
-
-        })
-
+const app = new Vue({
+		  el: '#app',
+		  data: {
+			currentPage: 'accueil'
+		  }
+		})
