453987b52fe4a3024663d1e7865243a77acef3de
Brooked <dauguetjulien@gmail.com>
2018-11-12 11:17:50 +0100

    ajout composants

---
+++ projetweb/components/header.component.js
@@ -0,0 +1,16 @@
+;(function () {
+  'use strict'
+
+  const template = `
+<section id="header">
+  <meta charset="UTF-8">
+  	<link rel="stylesheet" href="css/bootstrap.css">
+  	<link rel="stylesheet" href="stylesheet.css">
+    <title>Mes couilles en short</title>
+</section>
+  `
+
+  Vue.component('header', {
+    template: template
+  })
+})()
